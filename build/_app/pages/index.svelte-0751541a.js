9ca7b868790b7df78736158dc9f606d73f0cf77f
Bert Spaan <bert.spaan@gmail.com>
2021-10-14 10:19:23 +0200

    added build

---
+++ build/_app/pages/index.svelte-0751541a.js
@@ -0,0 +1,49 @@
+import{S as W,i as X,s as Z,e as o,t as i,k as g,c,a as d,g as p,d as s,n as _,b as n,J as tt,f as G,H as t,I as O,j as et,K as at,m as st,o as nt,x as lt,u as rt,v as ot}from"../chunks/vendor-b7f0cdcb.js";function it(C){let e,m,b,l,f,y,j,v,k,R,u,T,E,I,q,z,h,H,x,A,D,S,w,F,B,r,N;return{c(){e=o("div"),m=o("span"),b=i("B"),l=g(),f=o("span"),y=i("e"),j=g(),v=o("span"),k=i("r"),R=g(),u=o("span"),T=i("t"),E=g(),I=o("span"),q=i("S"),z=g(),h=o("span"),H=i("p"),x=g(),A=o("span"),D=i("a"),S=g(),w=o("span"),F=i("a"),B=g(),r=o("span"),N=i("n"),this.h()},l(P){e=c(P,"DIV",{class:!0,style:!0});var a=d(e);m=c(a,"SPAN",{class:!0});var J=d(m);b=p(J,"B"),J.forEach(s),l=_(a),f=c(a,"SPAN",{class:!0});var K=d(f);y=p(K,"e"),K.forEach(s),j=_(a),v=c(a,"SPAN",{class:!0});var V=d(v);k=p(V,"r"),V.forEach(s),R=_(a),u=c(a,"SPAN",{class:!0});var L=d(u);T=p(L,"t"),L.forEach(s),E=_(a),I=c(a,"SPAN",{class:!0});var $=d(I);q=p($,"S"),$.forEach(s),z=_(a),h=c(a,"SPAN",{class:!0});var M=d(h);H=p(M,"p"),M.forEach(s),x=_(a),A=c(a,"SPAN",{class:!0});var Y=d(A);D=p(Y,"a"),Y.forEach(s),S=_(a),w=c(a,"SPAN",{class:!0});var Q=d(w);F=p(Q,"a"),Q.forEach(s),B=_(a),r=c(a,"SPAN",{class:!0});var U=d(r);N=p(U,"n"),U.forEach(s),a.forEach(s),this.h()},h(){n(m,"class","B svelte-17debol"),n(f,"class","E svelte-17debol"),n(v,"class","R svelte-17debol"),n(u,"class","T svelte-17debol"),n(I,"class","S svelte-17debol"),n(h,"class","P svelte-17debol"),n(A,"class","A1 svelte-17debol"),n(w,"class","A2 svelte-17debol"),n(r,"class","N svelte-17debol"),n(e,"class","grid-container svelte-17debol"),tt(e,"--width",C[0])},m(P,a){G(P,e,a),t(e,m),t(m,b),t(e,l),t(e,f),t(f,y),t(e,j),t(e,v),t(v,k),t(e,R),t(e,u),t(u,T),t(e,E),t(e,I),t(I,q),t(e,z),t(e,h),t(h,H),t(e,x),t(e,A),t(A,D),t(e,S),t(e,w),t(w,F),t(e,B),t(e,r),t(r,N)},p(P,[a]){a&1&&tt(e,"--width",P[0])},i:O,o:O,d(P){P&&s(e)}}}function ct(C,e,m){let{width:b="175px"}=e;return C.$$set=l=>{"width"in l&&m(0,b=l.width)},[b]}class dt extends W{constructor(e){super();X(this,e,ct,it,Z,{width:0})}}function pt(C){let e,m,b,l,f,y,j,v,k,R,u,T,E,I,q,z,h,H,x,A,D,S,w,F,B;return y=new dt({}),{c(){e=o("style"),m=i(`* {
+			box-sizing: border-box;
+		}
+
+		body {
+			color: #111;
+			font-family: 'Roboto', sans-serif;
+			margin: 0;
+			padding: 1em;
+			position: absolute;
+			width: 100%;
+			height: 100%;
+		}
+
+		#svelte {
+			position: relative;
+			width: 100%;
+			height: 100%;
+
+			display: flex;
+			align-items: center;
+			justify-content: center;
+		}`),b=g(),l=o("main"),f=o("div"),et(y.$$.fragment),j=g(),v=o("p"),k=i("Hi, I'm Bert Spaan. I design and build creative technology for data and exploration."),R=g(),u=o("p"),T=i("I\u2019m currently building "),E=o("a"),I=i("Allmaps"),q=i(`,
+	open source tools for georeferencing, warping and exploring IIIF maps.`),z=g(),h=o("p"),H=i(`If you\u2019d like to get in touch,
+		you can send me an `),x=o("a"),A=i("email"),D=i(" or a message on "),S=o("a"),w=i("Twitter"),F=i("."),this.h()},l(r){const N=at('[data-svelte="svelte-6qjcjz"]',document.head);e=c(N,"STYLE",{});var P=d(e);m=p(P,`* {
+			box-sizing: border-box;
+		}
+
+		body {
+			color: #111;
+			font-family: 'Roboto', sans-serif;
+			margin: 0;
+			padding: 1em;
+			position: absolute;
+			width: 100%;
+			height: 100%;
+		}
+
+		#svelte {
+			position: relative;
+			width: 100%;
+			height: 100%;
+
+			display: flex;
+			align-items: center;
+			justify-content: center;
+		}`),P.forEach(s),N.forEach(s),b=_(r),l=c(r,"MAIN",{class:!0});var a=d(l);f=c(a,"DIV",{class:!0});var J=d(f);st(y.$$.fragment,J),J.forEach(s),j=_(a),v=c(a,"P",{class:!0});var K=d(v);k=p(K,"Hi, I'm Bert Spaan. I design and build creative technology for data and exploration."),K.forEach(s),R=_(a),u=c(a,"P",{class:!0});var V=d(u);T=p(V,"I\u2019m currently building "),E=c(V,"A",{href:!0,class:!0});var L=d(E);I=p(L,"Allmaps"),L.forEach(s),q=p(V,`,
+	open source tools for georeferencing, warping and exploring IIIF maps.`),V.forEach(s),z=_(a),h=c(a,"P",{class:!0});var $=d(h);H=p($,`If you\u2019d like to get in touch,
+		you can send me an `),x=c($,"A",{href:!0,class:!0});var M=d(x);A=p(M,"email"),M.forEach(s),D=p($," or a message on "),S=c($,"A",{href:!0,class:!0});var Y=d(S);w=p(Y,"Twitter"),Y.forEach(s),F=p($,"."),$.forEach(s),a.forEach(s),this.h()},h(){n(f,"class","logo svelte-1ag48ht"),n(v,"class","svelte-1ag48ht"),n(E,"href","https://allmaps.org"),n(E,"class","svelte-1ag48ht"),n(u,"class","svelte-1ag48ht"),n(x,"href","mailto:hello@bertspaan.nl"),n(x,"class","svelte-1ag48ht"),n(S,"href","https://twitter.com/bertspaan"),n(S,"class","svelte-1ag48ht"),n(h,"class","svelte-1ag48ht"),n(l,"class","svelte-1ag48ht")},m(r,N){t(document.head,e),t(e,m),G(r,b,N),G(r,l,N),t(l,f),nt(y,f,null),t(l,j),t(l,v),t(v,k),t(l,R),t(l,u),t(u,T),t(u,E),t(E,I),t(u,q),t(l,z),t(l,h),t(h,H),t(h,x),t(x,A),t(h,D),t(h,S),t(S,w),t(h,F),B=!0},p:O,i(r){B||(lt(y.$$.fragment,r),B=!0)},o(r){rt(y.$$.fragment,r),B=!1},d(r){s(e),r&&s(b),r&&s(l),ot(y)}}}class ut extends W{constructor(e){super();X(this,e,null,pt,Z,{})}}export{ut as default};
